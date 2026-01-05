17ba96e707aefd8bf9c5087fc62764e11868723c
Marija Đikanović <marija.djikanovic@gmail.com>
2026-01-05 18:12:34 +0100

    o nama

--- klijent/src/stranice/ONama.jsx
+++ klijent/src/stranice/ONama.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import Naslov from "../komponente/Naslov";
+import yettel from "../slike/logoi/yettel.jpg";
+import raifaisen from "../slike/logoi/raifaisen.png";
+import cocacola from "../slike/logoi/coca-cola.png";
 
 const ONama = () => {
     return (
         <div>
-            O nama
+            <Naslov naslov="O nama" podnaslov="Saznajte više o našoj misiji i timu" />
+            <p>
+                Dobrodošli na našu platformu za oglašavanje poslova! Naša misija je povezati poslodavce sa
+                kvalifikovanim kandidatima i olakšati proces zapošljavanja za sve uključene strane.
+            </p>
+            <p>
+                Naš tim čine iskusni profesionalci iz oblasti ljudskih resursa, tehnologije i marketinga,
+                posvećeni pružanju najbolje moguće usluge našim korisnicima. Verujemo u transparentnost,
+                efikasnost i inovacije kao ključne vrednosti koje nas vode u našem radu.
+            </p>
+            <p>
+                Hvala vam što ste deo naše zajednice. Radujemo se što ćemo zajedno graditi uspešne karijere
+                i poslovne prilike!
+            </p>
+
+            <Naslov naslov="Naši partneri" />
+
+            <div className="d-flex justify-content-around align-items-center mt-4">
+                <div className="text-center">
+                    <img src={yettel} alt="Yettel" style={{ maxWidth: '150px', height: 'auto' }} />
+                    <p>Yettel</p>
+                </div>
+                <div className="text-center">
+                    <img src={raifaisen} alt="Raiffeisen Bank" style={{ maxWidth: '150px', height: 'auto' }} />
+                    <p>Raiffeisen Bank</p>
+                </div>
+                <div className="text-center">
+                    <img src={cocacola} alt="Coca-Cola" style={{ maxWidth: '150px', height: 'auto' }} />
+                    <p>Coca-Cola</p>
+                </div>
+            </div>
+
         </div>
     );
 };
